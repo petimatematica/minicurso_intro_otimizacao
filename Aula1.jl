352204db5419e3728279779d00e60327b8bc3e7a
Emanuel Mendes Queiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-04-23 21:31:45 -0300

    Add files via upload

---
+++ Aula1.jl
@@ -0,0 +1,39 @@
+
+#Linguagem de programação julia
+
+#Operações básicas 
+ 
+# 1+1
+# 25/9
+# 8-90
+# 89^20
+
+#Programa básico
+
+# println("Olá, mundo!")
+
+
+# Atribuição
+# Uma declaração de atribuição cria uma nova variável e da à ela um valor
+
+# x = 245
+
+# n_val= π
+
+#Funções 
+
+# function area(raio)
+#     a = π * raio^2
+#     return a
+# end
+
+
+# function valorAbsoluto(x)
+#     if x < 0
+#         return -x
+#     else
+#         return x
+#     end
+# end
+
+
